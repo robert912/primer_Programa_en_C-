e1bef7471d73c9762c66443d3dfb3d5f00d3d0f0
Roberto O <roberto.orellana.t@usach.cl>
2018-12-18 18:03:14 -0300

    Indexadores

--- campos_y_Propiedades/campos_y_Propiedades/Program.cs
+++ campos_y_Propiedades/campos_y_Propiedades/Program.cs
@@ -12,8 +12,14 @@ namespace campos_y_Propiedades
         {
             //se crea la instancia(objeto) de la clase Vector
             var v2 = new Vector(new List<int> { 1, 2 });
-            Console.WriteLine(v2.Valores);
-            
+            foreach(int componentes in v2.Valores)
+            Console.Write("{0}, ",componentes);
+            Console.WriteLine("\n"+v2.dimension);
+            v2.Nombre = "Vector 1";
+            Console.WriteLine(v2.Nombre);
+            v2[0] = 5;
+            Console.WriteLine(v2[0]);
+            Console.ReadKey();
         }
     }
 
@@ -31,10 +37,28 @@ namespace campos_y_Propiedades
             }
         }
 
+        public int dimension
+        {
+            get
+            {
+                return _Valores.Count;
+            }
+        }
+
+        public string Nombre { get; set; }
+
+        //Indexadores
+        public int this[int i]
+        {
+            get { return _Valores[1]; }
+            set { _Valores[i] = value; }
+        }
+
         //Constructor
         public Vector(List<int> valores)
         {
             _Valores = valores;
         }
+
     }
 }
